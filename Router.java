5277221cd8ebb5f1dab94331ee9016d9252dd570
p0wndizz7e <andrewsa@lafayette.edu>
2018-04-26 15:47:54 -0400

    Cheese Creamers

--- Router.java
+++ Router.java
@@ -13,41 +13,167 @@ public class Router
     private int port;
     private String ip;
     private ArrayList<peerInfo> adjacentRouters; //The list of routers that are directly connected
-    private ArrayList<peerInfo> connectedRouters; //List of routers that have paths to
-    private ArrayList<routes> paths;
+    private ArrayList<peerInfo> connectedRouters; //List of routers that have a path to
+    private ArrayList<routes> forwardingTable;
     /**
      * Constructor for objects of class Node
      */
-    public Router(String fileName)
+    public static void main(String[] args)
+    {
+        try
+        {
+            Router obj = new Router();
+            obj.run(args);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void run (String[] args)
     {
         // initialise instance variables
         adjacentRouters = new ArrayList<peerInfo>();
         connectedRouters = new ArrayList<peerInfo>();
         
-        paths = new ArrayList<routes>();
-        readFile(fileName);
+        forwardingTable = new ArrayList<routes>();
+        readFile(args[0]);
+        
         SendUpdate sendUpdate = new SendUpdate();
+        ReceiveUpdate receiveUpdate = new ReceiveUpdate();
         sendUpdate.run();
+        receiveUpdate.run();
     }
-
     
+    //convert the list calculated distance vectors to a byte array 
+    //This is done, stop looking at it
     public byte[] convertToByteArray(ArrayList<peerInfo> connectedRouters)
     {
         byte[] data = new byte[2048];
         int index = 0;
+        String temp = "";
         for(peerInfo r : connectedRouters)
         {
-            String temp = r.ip + "," + r.port + "," + r.weight + ":";
-            byte[] byteConversion = temp.getBytes();
+            temp += r.ip + "," + r.port + "," + r.weight + ":";
         }
+        data = temp.getBytes();
         return data;
     }
     
-    public ArrayList<routes> convertToArrayList(byte[] byteArray)
+
+	
+    //Converts a recieved message, to an array list
+    public ArrayList<peerInfo> convertToPeerInfoArray(byte[] byteArray)
     {
-        return paths;
+        ArrayList<peerInfo> receivedDistances = new ArrayList<peerInfo>();
+        String temp = new String(byteArray);
+        String[] tokens = temp.split(":");
+        for(int x = 0; x < tokens.length - 1; x++)
+        {
+            System.out.print(tokens[x] + " ^ ");
+            String[] parts = tokens[x].split(",");
+            peerInfo link = new peerInfo();
+            System.out.print(parts[0] + " ? ");
+            System.out.print(parts[1] + " ? ");
+            System.out.print(parts[2] + " ? ");
+            
+            link.ip = parts[0];
+            link.port = Integer.parseInt(parts[1]);
+            link.weight = Integer.parseInt(parts[2]);
+            receivedDistances.add(link);
+        }
+        System.out.println("Cheese Credits");
+        for(peerInfo p : receivedDistances)
+        {
+            System.out.println(p.toString());
+        }
+        return receivedDistances;
     }
     
+    //5 before 12
+    public ArrayList<peerInfo> calculateDistance(ArrayList<peerInfo> recievedDistances)
+    {
+        //Need to calculate new distances
+        for(peerInfo p : recievedDistances)
+		{
+			
+		}
+        //Updated forwarding table
+        return connectedRouters;
+    }
+    
+    
+    
+    public class SendUpdate implements Runnable
+    {
+        @Override
+        public void run()
+        { 
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("TimerTask executing");
+                    try{
+                        for(peerInfo a : adjacentRouters)
+                        {
+                            DatagramSocket socket = new DatagramSocket();
+                            //Neeeds to be changed when using actual IP's
+                            InetAddress IPAddress = InetAddress.getByName("localhost"); 
+                            byte[] sendData = convertToByteArray(connectedRouters);
+                            DatagramPacket sendPacket = new DatagramPacket
+                                                        (sendData, sendData.length, IPAddress, a.port);
+                            socket.send(sendPacket);
+                            
+                        }
+                    }catch(Exception e )
+                    {
+                        e.printStackTrace();
+                        System.out.println("Error sending datagrams");
+                    }
+                }
+            };
+            Timer timer = new Timer("MyTimer");//create a new Timer
+    
+            timer.scheduleAtFixedRate(timerTask, 0, 3000);//this line starts the timer at the same time its executed
+        }
+    }
+    
+    public class ReceiveUpdate implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            try{
+                DatagramSocket socket = new DatagramSocket(port);
+                byte[] receiveData = new byte[2028];
+                while(true)
+                {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    socket.receive(receivePacket);
+                    System.out.println(receivePacket);
+                    ArrayList<peerInfo> nodes =  convertToPeerInfoArray(receivePacket.getData());
+                    //socket.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public class ReadCommands implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            
+        }
+    }
+    
+    public ArrayList<peerInfo> sortPeerInfo(ArrayList<peerInfo> arrayToSort)
+    {
+        return arrayToSort;
+    }
+    
+    //Sets up adjacent routers from the given text file
     public void readFile(String fileName)
     {
         ArrayList<String> messageArray = new ArrayList<String>();
@@ -71,61 +197,21 @@ public class Router
             info.port = Integer.parseInt(tokens[1]);
             info.weight = Integer.parseInt(tokens[2]);
             adjacentRouters.add(info);
-        }
-    }
-    
-    public class SendUpdate implements Runnable
-    {
-        public void run()
-        { 
-            TimerTask timerTask = new TimerTask() {
-                @Override
-                public void run() {
-                    System.out.println("TimerTask executing");
-                    try{
-                        for(peerInfo a : adjacentRouters)
-                        {
-                            DatagramSocket clientSocket = new DatagramSocket();
-                            InetAddress IPAddress = InetAddress.getByName("hostname");
-                            byte[] sendData = new byte[1024];
-                            //sendData = paths.getBytes();
-                        }
-                    }catch(Exception e )
-                    {
-                        System.out.println("Error sending datagrams");
-                    }
-                }
-            };
-            Timer timer = new Timer("MyTimer");//create a new Timer
-    
-            timer.scheduleAtFixedRate(timerTask, 0, 3000);//this line starts the timer at the same time its executed
-        }
-    }
-    
-    public class recieveUpdate implements Runnable
-    {
-        
-        public void run()
-        {
-            
-        }
-    }
-    
-    public class ReadCommands implements Runnable
-    {
-        
-        public void run()
-        {
-            
+            connectedRouters.add(info);
         }
     }
 }
 
 class peerInfo
 {
+    public String ip;
     public int port;
     public int weight;
-    public String ip;
+    
+    public String toString()
+    {
+        return ip + ":" + port + " W>" + weight;
+    }
 }
 
 class routes
